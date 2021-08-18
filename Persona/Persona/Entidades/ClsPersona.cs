8e891706e267c4112ecc3e301841d74e78b0b22a
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-08-17 21:19:17 -0600

    Actualiza1FS

--- Persona/Persona/Entidades/ClsPersona.cs
+++ Persona/Persona/Entidades/ClsPersona.cs
@@ -8,5 +8,26 @@ namespace Persona.Entidades
 {
     class ClsPersona
     {
+        private String nombre;
+        private String apellido;
+        private String cedula;
+        private String edad;
+        private String sexo;
+        private String colordePiel;
+        private String colordeOjos;
+        private String peso;
+        private String altura;
+        private String nacionalidad;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Cedula { get => cedula; set => cedula = value; }
+        public string Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string ColordePiel { get => colordePiel; set => colordePiel = value; }
+        public string ColordeOjos { get => colordeOjos; set => colordeOjos = value; }
+        public string Peso { get => peso; set => peso = value; }
+        public string Altura { get => altura; set => altura = value; }
+        public string Nacionalidad { get => nacionalidad; set => nacionalidad = value; }
     }
 }
