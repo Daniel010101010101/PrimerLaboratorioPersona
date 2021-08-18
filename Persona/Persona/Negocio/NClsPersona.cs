643237f32ea6aa90684db638a95fbf5f16f91745
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2021-08-17 21:39:33 -0600

    Funciones DT

--- Persona/Persona/Negocio/NClsPersona.cs
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
+       
+
+        public String Hablar(ClsPersona persona)
+        {
+            return "La Persona " + persona.Nombre + "esta hablando sobre la segunda guerra mundial ";
+        }
+        public String Caminar(ClsPersona persona)
+        {
+            return "La Persona " + persona.Nombre + "Esta caminando por el barrio ";
+        }
+        public String Reir(ClsPersona persona)
+        {
+            return "La Persona " + persona.Nombre + "Esta riendo con Franklin ";
+        }
+        public String Observar(ClsPersona persona)
+        {
+            return "La Persona " + persona.Nombre + "Esta observando a su Crush UwU ";
+        }
+
     }
 }
